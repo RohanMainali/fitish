96870b4124ab0ea7c66d5a355eab0c2a28a7d7cf
Rohan Mainali <rohanmainali@gmail.com>
2025-07-05 09:10:23 +0545

    Initial config by Rohan

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// config.php - Secure constants
+return [
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'fitishpro',
+    'DB_USER' => 'root',
+    'DB_PASS' => '',
+    'ALLOW_REGISTRATION' => true,
+    'SUPERADMIN_SETUP_KEY' => '1234567890'
+];
